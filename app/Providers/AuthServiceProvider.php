3a4276e0bd8c196e8a6bf0ec9142ec2442109248
真 菊池 <kikuchi@attract-flow.com>
2025-03-08 12:01:27 +0900

    Resolve merge conflict in AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,30 +2,18 @@
 
 namespace App\Providers;
 
-<<<<<<< HEAD
-// use Illuminate\Support\Facades\Gate;
-=======
 use Illuminate\Auth\Notifications\ResetPassword;
->>>>>>> b7b2b51 (Initial commit: Laravel project setup)
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
     /**
-<<<<<<< HEAD
-     * The model to policy mappings for the application.
-=======
      * The policy mappings for the application.
->>>>>>> b7b2b51 (Initial commit: Laravel project setup)
      *
      * @var array<class-string, class-string>
      */
     protected $policies = [
-<<<<<<< HEAD
-        //
-=======
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
->>>>>>> b7b2b51 (Initial commit: Laravel project setup)
     ];
 
     /**
@@ -33,15 +21,10 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-<<<<<<< HEAD
-=======
         $this->registerPolicies();
 
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
-
->>>>>>> b7b2b51 (Initial commit: Laravel project setup)
-        //
     }
 }
